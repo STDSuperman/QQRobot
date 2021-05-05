b032cba2ab20e9db3aaa6e145e9ff60abaa29e3c
STDSuperman <2750556766@qq.com>
2021-05-05 18:14:41 +0800

    :loud_sound: feat: 添加调用mirai api 日志

--- src/bot-send/send.service.ts
+++ src/bot-send/send.service.ts
@@ -1,22 +1,44 @@
 import { Injectable, HttpService } from '@nestjs/common';
 import { map } from 'rxjs/operators';
 import { SendGroupMessage, SendFriendMessage } from './interface/send.interface'
+import { LoggerService } from '@/logger/logger.service'
 
 @Injectable()
 export class BotSendService {
-    constructor(private http: HttpService) {}
+    constructor(
+        private http: HttpService,
+        private logger: LoggerService
+    ) {}
 
     // 发送群消息
     async sendGroupMessage(messageData: SendGroupMessage): Promise<boolean> {
         return this.http.post('/sendGroupMessage', messageData).pipe(
             map(res => res.data.code === 0)
-        ).toPromise();
+        ).toPromise()
+        .catch((e) => {
+            const logObj = {
+                type: 'sendGroupMessage',
+                message: e.message || JSON.stringify(e),
+                payload: messageData
+            }
+            this.logger.error(JSON.stringify(logObj));
+            return false;
+        });;
     }
 
     // 发送好友消息
     async sendFriendMessage(messageData: SendFriendMessage): Promise<boolean> {
         return this.http.post('/sendFriendMessage', messageData).pipe(
             map(res => res.data.code === 0)
-        ).toPromise();
+        ).toPromise()
+        .catch((e) => {
+            const logObj = {
+                type: 'sendFriendMessage',
+                message: e.message || JSON.stringify(e),
+                payload: messageData
+            }
+            this.logger.error(JSON.stringify(logObj));
+            return false;
+        });
     }
 }
