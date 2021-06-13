89294889147d95fd44bbd55395e0590a56e33710
STDSuperman <2750556766@qq.com>
2021-06-13 19:43:17 +0800

    :wrench: chore: 添加自定义git commit规则

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,6 +1,19 @@
 module.exports = {
-    extends: [
-      '@commitlint/config-conventional'
+	extends: ['cz'],
+	rules: {
+		"body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"],
+    "header-max-length": [1, "always", 71],
+    "scope-case": [1, "never", "lower-case"],
+    "subject-case": [
+      1,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
     ],
-    rules: {}
+    "subject-empty": [1, "never"],
+    "subject-full-stop": [1, "never", "."],
+    "type-case": [1, "always", "lower-case"],
+    "type-empty": [1, "never"],
+    "type-enum": [1, "always", []]
+	}
 };
